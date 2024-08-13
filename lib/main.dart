1450d22d2fafd053b5ee77d882604439669748d8
vigneshwari-22 <vigneshwarimohan2522@gmail.com>
2024-08-13 16:42:53 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,241 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'Q1. Who is the prime minister of India?',
+      'answers': [
+        {'text': 'Mahendra Singh Dhoni', 'score': -2},
+        {'text': 'Nethaji', 'score': -2},
+        {'text': 'Modi', 'score': 10},
+        {'text': 'Gandhi', 'score': -2},
+      ],
+    },
+    {
+      'questionText': 'Q2. What is Flutter?',
+      'answers': [
+        {'text': 'Android Development Kit', 'score': -2},
+        {'text': 'IOS Development Kit', 'score': -2},
+        {'text': 'Web Development Kit', 'score': -2},
+        {
+          'text':
+              'SDK to build beautiful IOS, Android, Web & Desktop Native Apps',
+          'score': 10
+        },
+      ],
+    },
+    {
+      'questionText': 'Q3. Which programming language is used by Flutter?',
+      'answers': [
+        {'text': 'Ruby', 'score': -2},
+        {'text': 'Dart', 'score': 10},
+        {'text': 'C++', 'score': -2},
+        {'text': 'Kotlin', 'score': -2},
+      ],
+    },
+    {
+      'questionText': 'Q4. Who created Dart programming language?',
+      'answers': [
+        {'text': 'Lars Bak and Kasper Lund', 'score': 10},
+        {'text': 'Brendan Eich', 'score': -2},
+        {'text': 'Bjarne Stroustrup', 'score': -2},
+        {'text': 'Jeremy Ashkenas', 'score': -2},
+      ],
+    },
+    {
+      'questionText':
+          'Q5. Is Flutter for Web and Desktop available in a stable version?',
+      'answers': [
+        {'text': 'Yes', 'score': -2},
+        {'text': 'No', 'score': 10},
+      ],
+    },
+  ];
+
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    if (_questionIndex < _questions.length) {
+      print('We have more questions!');
+    } else {
+      print('No more questions!');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Zitu'),
+          backgroundColor: Color.fromARGB(255, 134, 68, 185),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: _questionIndex < _questions.length
+              ? Quiz(
+                  answerQuestion: _answerQuestion,
+                  questionIndex: _questionIndex,
+                  questions: _questions,
+                )
+              : Result(_totalScore, _resetQuiz),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Question extends StatelessWidget {
+  final String questionText;
+
+  const Question(this.questionText, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: const TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
+
+class Quiz extends StatelessWidget {
+  final List<Map<String, Object>> questions;
+  final int questionIndex;
+  final Function answerQuestion;
+
+  const Quiz({
+    Key? key,
+    required this.questions,
+    required this.answerQuestion,
+    required this.questionIndex,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Question(
+          questions[questionIndex]['questionText'].toString(),
+        ),
+        ...(questions[questionIndex]['answers'] as List<Map<String, Object>>)
+            .map((answer) {
+          return Answer(
+              () => answerQuestion(answer['score']), answer['text'].toString());
+        }).toList()
+      ],
+    );
+  }
+}
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetHandler;
+
+  const Result(this.resultScore, this.resetHandler, {Key? key})
+      : super(key: key);
+
+  String get resultPhrase {
+    String resultText;
+    if (resultScore >= 41) {
+      resultText = 'You are awesome!';
+      print(resultScore);
+    } else if (resultScore >= 31) {
+      resultText = 'Pretty likeable!';
+      print(resultScore);
+    } else if (resultScore >= 21) {
+      resultText = 'You need to work more!';
+    } else if (resultScore >= 1) {
+      resultText = 'You need to work hard!';
+    } else {
+      resultText = 'This is a poor score!';
+      print(resultScore);
+    }
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: const TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            'Score $resultScore',
+            style: const TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          TextButton(
+            onPressed: () => resetHandler(),
+            child: Container(
+              color: Colors.green,
+              padding: const EdgeInsets.all(14),
+              child: const Text(
+                'Restart Quiz',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Answer extends StatelessWidget {
+  final Function selectHandler;
+  final String answerText;
+
+  const Answer(this.selectHandler, this.answerText, {Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () => selectHandler(),
+        style: ButtonStyle(
+            textStyle:
+                MaterialStateProperty.all(const TextStyle(color: Colors.white)),
+            backgroundColor: MaterialStateProperty.all(Colors.green)),
+        child: Text(answerText),
+      ),
+    );
+  }
+}
